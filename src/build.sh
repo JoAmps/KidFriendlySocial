165a9735ca8960fb5a89e3b83d7d9f9a04f70bcc
Hyacinth Ampadu <ampaduh@gmail.com>
2023-03-19 18:54:18 +0000

    adding files especially for monitoring

--- src/build.sh
+++ src/build.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
-docker login
-docker build --tag joamps/app:latest . -f Dockerfile
-docker push joamps/app:latest
+doctl registry login
+docker build --platform=linux/x86_64 -t app:latest . -f Dockerfile
+docker tag app:latest registry.digitalocean.com/kidfriendlysocial/app:latest
+docker push registry.digitalocean.com/kidfriendlysocial/app:latest
